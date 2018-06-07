f6f6b88519efe6aa8a8cfb28af6d8a9577f0bc62
Puzolifer <flashback2018@mail.ru>
2018-06-07 19:39:58 +0700

    start code

---
+++ src/main.cpp
@@ -0,0 +1,9 @@
+using namespace std;
+int main() {
+	const int massive[4][4] = {{1,2,3,4},{5,6,7,8},{9,10,11,12},{13,14,15,0}};
+	int mass[4][4] = {{1,2,3,4},{5,6,7,8},{9,10,11,12},{13,14,15,0}};
+	int x = 3;
+	int y = 3;
+	int perk = 0;
+	srand(time(NULL));
+	initwindow(600,600);
